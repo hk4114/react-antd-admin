33db072f28930734f3e9bda7cdfc28524d6220b5
huakang <741180788@qq.com>
2022-08-01 18:03:29 +0800

    ✨新增(custom): 修改

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,40 +1,71 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { Breadcrumb, Layout, Menu } from 'antd'
+import React, { useState } from 'react'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined
+} from '@ant-design/icons'
+import { Outlet, Link } from 'react-router-dom'
+import { Layout, Menu } from 'antd' //  Breadcrumb,
 import './index.less'
 
-const { Header, Content, Footer } = Layout
+const { Header, Content, Sider } = Layout // Footer,
 
 const LayoutIndex = () => {
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
-    <section className="layout">
-      <Layout className="layout">
-        <Header>
+    <section className="container">
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
           <div className="logo" />
           <Menu
             theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            items={new Array(15).fill(null).map((_, index) => {
-              const key = index + 1
-              return {
-                key,
-                label: `nav ${key}`
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: <Link to={'/404'}>nav 1</Link>
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2'
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3'
               }
-            })}
+            ]}
           />
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-content">
+        </Sider>
+        <Layout>
+          <Header>
+            {collapsed ? (
+              <MenuUnfoldOutlined
+                onClick={() => setCollapsed(!collapsed)}
+                className="f-white"
+              />
+            ) : (
+              <MenuFoldOutlined
+                onClick={() => setCollapsed(!collapsed)}
+                className="f-white"
+              />
+            )}
+            {/* {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+							className: "trigger",
+							onClick: () => setCollapsed(!collapsed)
+						})} */}
+          </Header>
+          <Content>
             <Outlet></Outlet>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Footer</Footer>
+          </Content>
+          {/* <Footer>Footer</Footer> */}
+        </Layout>
       </Layout>
     </section>
   )
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -3,7 +3,6 @@ import { Navigate, useRoutes } from 'react-router-dom'
 import Layout from '@/components/Layout'
 import lazyLoad from '@/components/Loading/lazyLoad'
 import Home from '@/pages/Home'
-import About from '@/pages/About'
 
 // 路由表 根据需要进行拆分
 // https://reactrouter.com/docs/en/v6
@@ -32,15 +31,24 @@ export const rootRouter = [
   },
   {
     path: '/about',
-    element: <About />,
+    element: lazyLoad(React.lazy(() => import('@/pages/About/index'))),
     meta: {
       title: 'About',
       key: 'about'
     }
   },
   {
+    path: '/404',
+    element: lazyLoad(React.lazy(() => import('@/components/ErrorPage/index'))),
+    meta: {
+      requiresAuth: false,
+      title: '404页面',
+      key: '404'
+    }
+  },
+  {
     path: '*',
-    element: <Navigate to="/" />
+    element: <Navigate to="/404" />
   }
 ]
 
